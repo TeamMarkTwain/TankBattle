f46a1da4816b8d9dbc410e7b1eb2a6095ded8a55
Slavi <slavicvetanov@gmail.com>
2014-02-21 11:13:21 +0200

    Update Engine.cs
    Little change. Different key, different action call

--- TestingStuff/Engine.cs
+++ TestingStuff/Engine.cs
@@ -23,7 +23,19 @@ namespace TankBattle
                 {
                     ConsoleKeyInfo pressedKey = Console.ReadKey(true);
 
-                    playerTank.Move(pressedKey);
+                    if (pressedKey.Key == ConsoleKey.UpArrow || pressedKey.Key == ConsoleKey.DownArrow ||
+                        pressedKey.Key == ConsoleKey.LeftArrow || pressedKey.Key == ConsoleKey.RightArrow)
+                    {
+                        playerTank.Move(pressedKey);
+                    }
+                    else if (pressedKey.Key == ConsoleKey.Spacebar)
+                    {
+                        //implement fire
+                    }
+                    else if (pressedKey.Key == ConsoleKey.Escape)
+                    {
+                        //implement pause  and  pause menu (exit, continue ... )
+                    }
                 }
             }
         }
