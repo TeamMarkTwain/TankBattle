ca9f1f847fa4bbdb51ebc212adfdd1bd5ee8735c
2She2 <yanev.tm@gmail.com>
2014-02-21 19:47:17 +0200

    Arch edit

---
+++ TestingStuff/FieldCoords.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TankBattle
+{
+    public struct FieldCoords
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public FieldCoords(int row, int col) : this()
+        {
+            this.X = row;
+            this.Y = col;
+        }
+
+        public static FieldCoords operator +(FieldCoords a, FieldCoords b)
+        {
+            return new FieldCoords(a.X + b.X, a.Y + b.Y);
+        }
+
+        public static FieldCoords operator -(FieldCoords a, FieldCoords b)
+        {
+            return new FieldCoords(a.X - b.X, a.Y - b.Y);
+        }
+
+        public override bool Equals(object obj)
+        {
+            FieldCoords objAsFieldCoords = (FieldCoords)obj;
+
+            return objAsFieldCoords.X == this.X && objAsFieldCoords.Y == this.Y;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.X.GetHashCode()*7 + this.Y;
+        }
+    }
+}
