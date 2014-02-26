7964d55ce20ef0629590e63522c2a3989d7dfd1b
2She2 <yanev.tm@gmail.com>
2014-02-26 22:29:44 +0200

    Changes in relation with inheritance of Tank

--- HitManager.cs
+++ HitManager.cs
@@ -93,24 +93,6 @@ namespace TankBattle
             return true;
         }
 
-        public static void ManageShotsAndTanks(List<CannonBall> shots, List<Tank> tanks)
-        {
-            for (int i = 0; i < shots.Count; i++)
-            {
-                for (int j = 0; j < tanks.Count; j++)
-                {
-                    if (IsShotPositionEqualTank(shots[i], tanks[j]))
-                    {
-                        int shotPower = shots[i].ShootPower;
-                        int targetHealth = tanks[j].Health;
-
-                        shots[i].LooseHealth(targetHealth);
-                        tanks[j].LooseHealth(shotPower);
-                    }
-                }
-            }
-        }
-
         public static void ManageShotsAndLevelObject(List<CannonBall> shots, List<LevelObject> targets)
         {
             foreach (var shot in shots)
@@ -150,30 +132,6 @@ namespace TankBattle
 
             int targetMinY = target.X;
             int targetMaxY = target.X + target.LoadVisual()[0].Length - 1;
-
-            if (shotX >= targetMinX && shotX <= targetMaxX &&
-                shotY >= targetMinY && shotY <= targetMaxY)
-            {
-                isHitted = true;
-            }
-
-            return isHitted;
-        }
-
-        private static bool IsShotPositionEqualTank(CannonBall shot, Tank tank)
-        {
-            bool isHitted = false;
-
-            string[] targetImage = tank.GetVisual();
-
-            int shotX = shot.X;
-            int shotY = shot.Y;
-
-            int targetMinX = tank.Y;
-            int targetMaxX = tank.Y + tank.GetVisual().Length - 1;
-
-            int targetMinY = tank.X;
-            int targetMaxY = tank.X + tank.GetVisual()[0].Length - 1;
 
             if (shotX >= targetMinX && shotX <= targetMaxX &&
                 shotY >= targetMinY && shotY <= targetMaxY)
